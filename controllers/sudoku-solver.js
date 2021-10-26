de9465d1341d2f45d7b2bb64f413d217aae56978
Daman Arora <damanarora724@gmail.com>
2021-10-26 19:16:42 +0530

    Started Unit testing

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,22 +1,113 @@
 class SudokuSolver {
 
+  // Validates a puzzle string
   validate(puzzleString) {
+    if(puzzleString.length !== 81){
+      return false;
+    }
+    let chars = [...puzzleString];
+    for(let c of chars){
+      if(isNaN(parseInt(c)) && c !== '.'){
+        return false;
+      }      
+    }
+    return true;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    value = `${value}`;
+    // For column numbers
+    for(let i = 0 ; i < 9; i ++){
+      let index = this.getIndex(row, i);
+            
+      // if it is already present in current row, then placement is not possible.
+      if(puzzleString[index] === value){
+        return false;
+      }
+    }
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    value = `${value}`;
+    // For row numbers
+    for (let i = 0; i < 9; i++) {
+      let index = this.getIndex(i, column);
+      console.log(value, "With", puzzleString[index]);
+      // if it is already present in current column, then placement is not possible.
+      if (puzzleString[index] === value) {
+        return false;
+      }
+    }
+    return true;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    value = `${value}`;
+    let rank = this.getRankOrStack(row);
+    let stack = this.getRankOrStack(column);
 
+    let rowPlus = rank * 3;
+    let colPlus = stack * 3;
+
+    for(let i = 0 ; i < 3; i ++){
+      for(let j = 0; j < 3; j ++){
+        let index = this.getIndex(i + rowPlus, j + colPlus);
+        
+        if(puzzleString[index] === value){
+          return false;
+        }
+      }
+    }
+    return true;
   }
 
   solve(puzzleString) {
-    
+
+    let toSolveIndex = puzzleString.indexOf('.');
+
+    let row = Math.floor(toSolveIndex/9);
+
+    let column = (toSolveIndex%9);
+
+    let possibleNext = false;
+    for(let i = 1; i < 9; i ++){
+      // i is number to be filled/checked.
+
+      if(this.checkRowPlacement(puzzleString, row, column, i) && 
+      this.checkColPlacement(puzzleString, row, column, i) &&
+      this.checkRegionPlacement(puzzleString, row, column, i)){
+        possibleNext = true;
+
+        // If placement is possible, then place it and call recursively.
+        var possibleString = new String(puzzleString);
+        let indexToFill = this.getIndex(row, column);
+        possible[indexToFill] = `${i}`;
+
+        let checkDeep = solve(possibleString);
+
+        // If recursive solution is not possible, that means this number doesn't belong here.
+        if(!checkDeep){
+          possibleNext = false;
+          continue;
+        }else{
+          // If recursive solution is possible, return the solution.
+          return checkDeep;
+        }
+
+      }
+    }
+    if(!possibleNext){
+      return false;
+    }
+  }
+
+  getIndex(row, column){
+    return (9*row)+column;
+  }
+
+  getRankOrStack(cordinate){
+    return Math.floor(cordinate/3);
   }
 }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,50 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver;
 
 suite('UnitTests', () => {
+    suite('Testing Validate method', ()=>{
+        test('With empty string => false', ()=>{
+            assert.equal(solver.validate(''), false);
+        });
 
+        test('With valid String => true', ()=>{
+            assert.equal(solver.validate('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'), true);
+        })
+
+        test('With invalid String => false', ()=>{
+            assert.equal(solver.validate('IAmABadString'), false);
+        });
+    });
+
+    suite('Testing row placement method', ()=>{
+        test('When number can be placed => true', ()=>{
+            assert.equal(solver.checkRowPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.',
+                0, 1, 3), true);
+            
+            assert.equal(solver.checkRowPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 
+                8, 8, 8), true);
+        })
+
+        test('When number cannot be placed => false', ()=>{
+            assert.equal(solver.checkRowPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.', 
+                0, 1, 2), false);
+            assert.equal(solver.checkRowPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.',
+                8, 8, 1), false);            
+        })
+    });
+
+    suite('Testing column placement method', ()=>{
+        test('When number can be placed => true', ()=>{
+            assert.equal(solver.checkColPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.',
+                1, 0, 9), true);
+            assert.equal(solver.checkColPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.',
+                1, 5, 8), true);
+        })
+        test('When number cannot be placed => false', ()=>{
+            assert.equal(solver.checkColPlacement('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.',
+                4, 0, 1), false);
+        })
+    })
 });
