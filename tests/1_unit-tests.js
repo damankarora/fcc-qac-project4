1f584c329e4dbfb483bcc2ecfdd2d850b5a87ab6
Daman Arora <damanarora724@gmail.com>
2021-10-29 13:48:27 +0530

    updated unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -18,11 +18,11 @@ suite('UnitTests', () => {
         })
 
         test('Logic handles a puzzle string that is not 81 characters in length', () => {
-            assert.throws(() => { solver.validate('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37..') });
+            assert.throws(() => { solver.validate('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37..') }, "Expected puzzle to be 81 characters long");
         })
 
         test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', ()=>{
-            assert.throws(() => { solver.validate('1.5..2.84..B3.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.')});
+            assert.throws(() => { solver.validate('1.5..2.84..B3.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.') }, "Invalid characters in puzzle");
         });
     });
 
